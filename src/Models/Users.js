c9b535f60d5cf317678c319e50b671fe8ccb7934
Maria Geyzianny de Sousa Silva <geizy@Marias-MacBook-Air.local>
2023-04-26 16:09:00 -0300

    login

--- src/Models/Users.js
+++ src/Models/Users.js
@@ -9,6 +9,14 @@ class User  {
             .select(['cpf', 'nome', 'data_nascimento', 'status_desconto', 'id', 'senha'])
             return clientes;
         }
+
+    async findByIdLogin(email){
+        return await knex('clientes')
+            .withSchema('public')
+            .select(['cpf'])
+            .where({ email })
+            .first();
+        }
 }
 
 module.exports = User;
--- src/Services/Auth.js
+++ src/Services/Auth.js
@@ -1,12 +1,29 @@
 const {default:axios} = require("axios");
 const Users = require("../Models/Users");
+const {knex} = require("knex");
+const knex = require ("../Database")
+
+const user = new Users()
 
 module.exports = {
+    
     login: async (req, res) => {
         const {nome, email, senha} = req.body;
-        const query = await knex('clientes').withSchema('public').where({nome, email, senha }).first();
-
-
+        try{        
+            if(request.status === 200){
+                const uuid = await user.findByIdLogin(user.email);
+                if(uuid){
+                    return res.status(200).send({success: true})
+                }
+            }
+            else{
+                return res.status(400).send({ success: false, message: 'Email ou senha inválidos' })
+                
+            }
+        }
+        catch (e) {
+            return res.status(400).send({ success: false, message: e})
+        }
     }
 }
 
